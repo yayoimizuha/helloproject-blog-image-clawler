c3f4e1c69754c174cead16769f957bce360b0ce3
片山　朋和 <poko.peko.tomo@icloud.com>
2021-11-23 16:51:43 +0900

    画像URLの表示をリスト型にしてpprintで表示。

--- main.py
+++ main.py
@@ -91,6 +91,9 @@ def download_image_link(url):
 
 
 time.sleep(3)
-for i in photo_url_list:
-    print("image url: " + i)
-    print(download_image_link(i))
+pprint(joblib.Parallel(n_jobs=N_JOBS, backend='threading')(
+    joblib.delayed(download_image_link)(url) for url in photo_url_list))
+
+# for i in photo_url_list:
+#    print("image url: " + i)
+#    print(download_image_link(i))
