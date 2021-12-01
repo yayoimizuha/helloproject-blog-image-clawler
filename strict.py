c11eaccb0f7bba5d912023a51448109eaf344705
片山　朋和 <poko.peko.tomo@icloud.com>
2021-12-01 14:20:45 +0900

    write up strict.py (have some error)

--- strict.py
+++ strict.py
@@ -15,21 +15,123 @@ ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 ssl._create_default_https_context = ssl._create_unverified_context
 
+N_JOBS = 40
+
 blog_list = ["angerme-ss-shin", "angerme-amerika", "angerme-new", "juicejuice-official", "tsubaki-factory",
              "morningmusume-10ki", "morningm-13ki", "morningmusume15ki", "morningmusume-9ki", "beyooooonds-rfro",
              "beyooooonds-chicatetsu", "beyooooonds"]
 
 
+def safe_request_get_as_text(url):
+    get_error = 0
+    while get_error == 0:
+        try:
+            text = requests.get(url).text
+            get_error += 1
+        except BaseException as error:
+            print("\n\n\n\n\n\n" + "Error occurred: " + str(error) + "\n\n\n\n\n\n")
+            get_error = 0
+        if get_error > 5:
+            continue
+
+
+def inspect_entry_list(url):
+    print(" Processing: " + url)
+    return BeautifulSoup(safe_request_get_as_text(url), 'html.parser').find('ul', {'class': 'skin-archiveList'}) \
+        .find_all('h2', {'data-uranus-component': 'entryItemTitle'})
+
+
 def diary_link_crawler(keyword):
     dairy_url_list = []
-    print(" Processing: " + keyword)
-    dairy_list = BeautifulSoup(requests.get(keyword).text, 'html.parser').find('ul', {'class': 'skin-archiveList'})
 
-    for dairy_list_html in dairy_list.find_all('h2', {'data-uranus-component': 'entryItemTitle'}):
-        dairy_url_list.append(
-            'https://ameblo.jp' + BeautifulSoup(str(dairy_list_html), 'html.parser').find('a')['href'])
-    return dairy_url_list
+    # Inspect page num
+    pagination_num = int(
+        re.search('entrylist-(.*?).html',
+                  BeautifulSoup(safe_request_get_as_text("https://ameblo.jp/" + keyword + "/entrylist.html"),
+                                'html.parser').find('a', {'data-uranus-component': 'paginationEnd'})['href']).group(1))
+    print("ページ数:" + str(pagination_num))
 
-# for i in blog_list:
-#     for j in diary_link_crawler(i):
-#         for k in
+    # Extract 1st page.
+    dairy_url_list.append(inspect_entry_list("https://ameblo.jp/" + keyword + "/entrylist.html"))
+
+    # Generating each page links
+    pagination_links = []
+    pagination_links.clear()
+    for x in range(2, int(pagination_num) + 1):
+        pagination_links.append("https://ameblo.jp/" + keyword + '/entrylist-' + str(x) + ".html")
+
+    # Crawl pages as parallel
+    dairy_url_list = joblib.Parallel(n_jobs=N_JOBS, backend="threading")(
+        joblib.delayed(inspect_entry_list)(keyword) for keyword in pagination_links)
+
+    # Return url array.(formatted)
+    return itertools.chain.from_iterable(itertools.chain.from_iterable(dairy_url_list))
+
+
+def image_detector(url):
+    page = safe_request_get_as_text(url)
+    image_class = BeautifulSoup(page, 'html.parser').find('div', {'data-uranus-component': 'entryBody'}) \
+        .find_all('img', class_='PhotoSwipeImage')
+
+    image_url = []
+
+    hashtag = str(re.search('"theme_name":".*?"', page)[0])
+    if hashtag == "":
+        hashtag = 'None'
+    hashtag = hashtag[14:-1]
+    iso_date = str(re.search('"dateModified":".*?"', page)[0])[16:-1]
+    count = 0
+    for images in image_class:
+        count += 1
+        if count % 2 == 0:
+            continue
+
+        image_url.append(
+            str(url).rsplit('/', 1)[0] + '/image-' + BeautifulSoup(str(images), 'html.parser')
+            .find('img')['data-entry-id'] + '-' + BeautifulSoup(str(images), 'html.parser').find('img')['data-image-id']
+            + '.html' + '#' + hashtag + '#' + str(iso_date) + '#' + BeautifulSoup(str(images), 'html.parser')
+            .find('img')['data-image-order'])
+
+    print("image_url[" + str(int(len(image_url))) + "]: \n" + pprint.PrettyPrinter(indent=4).pformat(image_url) + '\n')
+    return image_url
+
+
+def image_downloader(image_link):
+    if image_link is None:
+        return 'None'
+
+    # blog_idを取得
+    blog_id = str(re.search(".*?image-(\d+)-.*?", str(image_link)).group(1))
+    if blog_id is None:
+        blog_id = 'no_blog_id'
+
+    # ブログ内の画像番号を取得
+    image_order = str(image_link).split('#')[3]
+    if image_order is None:
+        image_order = str(1)
+
+    direct_image_link = BeautifulSoup(safe_request_get_as_text(image_link), 'html.parser') \
+        .find('main').find('img', {'aria-hidden': 'false'})['src']
+
+    filename = str(image_link).split('#')[1] + '=' + str(image_link).split('#')[0].split('/')[-2] + '=' + blog_id \
+               + '-' + image_order + '.jpg'
+    download_status = 0
+    while download_status == 0:
+        try:
+            urllib.request.urlretrieve(direct_image_link, filename)
+            download_status += 1
+        except BaseException as error:
+            print("\n\n\n\n\n\n" + "Error occurred: " + str(error) + "\n\n\n\n\n\n")
+        if download_status > 5:
+            continue
+
+    os.utime(path=filename,
+             times=(os.stat(path=filename).st_atime,
+                    datetime.datetime.fromisoformat(str(image_link).split('#')[2]).timestamp()))
+
+
+for i in blog_list:
+    for j in diary_link_crawler(i):
+        for k in image_detector(j):
+            image_downloader(k)
+            time.sleep(3)
