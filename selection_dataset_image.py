c512c54527fe08397a89d3f51ebb9e62eb9c33d0
片山　朋和 <poko.peko.tomo@icloud.com>
2022-01-28 17:36:50 +0900

    fix

--- selection_dataset_image.py
+++ selection_dataset_image.py
@@ -1,13 +1,15 @@
 import shutil
 import os
 
+SELECTION_DIR_NAME = 'face_dataset-2022-1-28'
+
 for dir_dataset in os.listdir(os.path.join(os.getcwd(), 'face_dataset')):
 
     if not os.path.isdir(os.path.join(os.getcwd(), 'face_dataset', dir_dataset)):
         continue
 
-    if not os.path.isdir(os.path.join(os.getcwd(), 'face_dataset-2021-12-27', dir_dataset)):
-        os.mkdir(os.path.join(os.getcwd(), 'face_dataset-2021-12-27', dir_dataset))
+    if not os.path.isdir(os.path.join(os.getcwd(), SELECTION_DIR_NAME, dir_dataset)):
+        os.mkdir(os.path.join(os.getcwd(), SELECTION_DIR_NAME, dir_dataset))
         print("Create dir: " + dir_dataset)
 
     print(len(os.listdir(os.path.join(os.getcwd(), 'face_dataset', dir_dataset))))
@@ -21,4 +23,4 @@ for dir_dataset in os.listdir(os.path.join(os.getcwd(), 'face_dataset')):
         print(dir_dataset, file)
 
         shutil.copy2(os.path.join(os.getcwd(), 'face_dataset', dir_dataset, file),
-                     os.path.join(os.getcwd(), 'face_dataset-2021-12-27', dir_dataset, file))
+                     os.path.join(os.getcwd(), SELECTION_DIR_NAME, dir_dataset, file))
