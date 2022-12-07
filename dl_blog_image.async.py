26a6336da511ba8fc77302b0fa8fcf9fc6440c87
yayoimizuha <poko.peko.tomo@icloud.com>
2022-12-07 18:35:04 +0900

    update

--- dl_blog_image.async.py
+++ dl_blog_image.async.py
@@ -1,12 +1,16 @@
 import pprint
 import re
-from bs4 import BeautifulSoup
-from aiohttp import ClientSession
-from itertools import chain
-from asyncio import gather, run, Semaphore
-from datetime import datetime
+import sys
 
-PARALLEL_LIMIT = 300
+from bs4 import BeautifulSoup
+from aiohttp import ClientSession, ClientConnectorError, TCPConnector
+from itertools import chain
+from asyncio import gather, run, Semaphore, sleep
+from datetime import datetime
+from aiofiles import open
+from os import path, getcwd, utime, stat
+
+PARALLEL_LIMIT = 100
 
 blog_list = ["angerme-ss-shin", "angerme-amerika", "angerme-new", "juicejuice-official", "tsubaki-factory",
              "morningmusume-10ki", "morningm-13ki", "morningmusume15ki", "morningmusume-9ki", "beyooooonds-rfro",
@@ -14,7 +18,7 @@ blog_list = ["angerme-ss-shin", "angerme-amerika", "angerme-new", "juicejuice-of
              "kumai-yurina-blog", "sudou-maasa-blog", "sugaya-risako-blog", "miyamotokarin-official",
              "kobushi-factory", "sayumimichishige-blog"]
 
-blog_list = ["ocha-norma"]
+blog_list = ["morningmusume-9ki"]
 
 request_header = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0'
@@ -23,7 +27,8 @@ request_header = {
 
 async def run_all() -> None:
     sem: Semaphore = Semaphore(PARALLEL_LIMIT)
-    session: ClientSession = ClientSession(trust_env=True, headers=request_header)
+    session: ClientSession = ClientSession(trust_env=True, headers=request_header,
+                                           connector=TCPConnector(use_dns_cache=True))
     list_pages_count = await gather(*[parse_list_pages_count(blog_name=blog_name) for blog_name in blog_list],
                                     return_exceptions=True)
     for name, pages in zip(blog_list, list_pages_count):
@@ -41,8 +46,11 @@ async def run_all() -> None:
         if 'html' not in url:
             print(url)
 
-    image_link = await gather(*[parse_image(url, sem, session) for url in url_list], return_exceptions=True)
-    pprint.pprint(list(chain.from_iterable(image_link)))
+    image_links = await gather(*[parse_image(url, sem, session) for url in url_list], return_exceptions=True)
+    image_link_package = list(chain.from_iterable(image_links))
+    pprint.pprint(image_link_package)
+
+    await gather(*[download_image(filename, url, date, sem, session) for filename, url, date in image_link_package])
 
     await session.close()
 
@@ -70,19 +78,25 @@ async def parse_list_page(blog_name: str, order: int, sem: Semaphore, session: C
     return url_list
 
 
-# filename , url ,date
 async def parse_image(url: str, sem: Semaphore, session: ClientSession) -> list[tuple[str, str, datetime]]:
-    async with sem:
-        async with session.get(url) as resp:
-            resp_html = await resp.text()
+    while True:
+        async with sem:
+            try:
+                async with session.get(url) as resp:
+                    resp_html = await resp.text()
+                    await sleep(1.0)
+                    break
+            except ClientConnectorError as e:
+                await sleep(5.0)
+                print(e, file=sys.stderr)
     theme = grep_theme(resp_html)
     date = datetime.fromisoformat(grep_modified_time(resp_html))
     blog_account = url.split('/')[-2]
     blog_entry = url.split('/')[-1].split('.')[0]
     parse = BeautifulSoup(resp_html, 'lxml')
-    print(theme, end='\t')
-    print(parse.find('title').text, end='\t')
-    print(grep_modified_time(resp_html))
+    print(theme + "　" * (8 - len(theme)), end='')
+    print(date.date(), end='\t')
+    print(parse.find('title').text)
     entry_body = parse.find('div', {'data-uranus-component': 'entryBody'})
     image_divs = entry_body.find_all('img', class_='PhotoSwipeImage')
     return_list = list()
@@ -92,7 +106,22 @@ async def parse_image(url: str, sem: Semaphore, session: ClientSession) -> list[
             str(div["src"]).split('?')[0],
             date
         ))
+    # filename , url ,date
     return return_list
+
+
+async def download_image(filename: str, url: str, date: datetime, sem: Semaphore, session: ClientSession) -> None:
+    filepath = path.join(getcwd(), "dl_await", filename)
+    if path.isfile(filepath):
+        print(f"file already downloaded.: {filename}")
+        return
+    async with sem:
+        print("download", url, sep=': ')
+        async with session.get(url) as resp:
+            data = await resp.read()
+    async with open(file=filepath, mode="wb") as f:
+        await f.write(data)
+    utime(path=filepath, times=(stat(path=filepath).st_atime, date.timestamp()))
 
 
 theme_regex = re.compile('"theme_name":"(.*?)"')
